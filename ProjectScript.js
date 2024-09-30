c8b9334a7b9c551b02d73dbd5249fa7397ee2fb3
JustBelal <mbilalimran98@gmail.com>
2024-09-30 18:34:15 +0500

    player choice is initialized on the basis of argument passed to function.

--- ProjectScript.js
+++ ProjectScript.js
@@ -1,3 +1,9 @@
-const check = () => {
-  console.log("Code is running");
-};
+let playerChoice;
+function selectChoice(choice) {
+  playerChoice = choice;
+  console.log("Player selected: " + playerChoice);
+}
+
+document.querySelectorAll(".icons img").forEach((img, index) => {
+  img.onclick = () => selectChoice(index + 1);
+});
